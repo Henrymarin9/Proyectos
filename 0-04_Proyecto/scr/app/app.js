730d14170356bff1a9fda92b8c180522a5038e07
Henry <97172666+Henrymarin9@users.noreply.github.com>
2023-07-19 00:20:18 -0300

    Formulario-FrontMenthor con JS

---
+++ 0-04_Proyecto/scr/app/app.js
@@ -0,0 +1,14 @@
+"use strict"
+const button = document.getElementById("button").addEventListener("click", function(){
+    document.getElementById("firstName");
+});
+
+const form = ()=>{
+    let array = []
+    array.push(firstName);
+    return array;
+
+}
+if(firstName.length > 0){ 
+    console.log(form());
+}
